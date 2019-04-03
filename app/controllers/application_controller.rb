72047779c583d31ef9beaade4c62c5f461dabf67
Grace Choi <oiograceoio@gmail.com>
2019-04-03 15:30:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(email: params["email"], password: params["password"])
+    @user = User.find_by(email: params["email"])
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
