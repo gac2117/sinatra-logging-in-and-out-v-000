9dbb61a0259025becf99c257ad2da9d1b8c03050
Grace Choi <oiograceoio@gmail.com>
2019-04-03 15:55:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   get '/account' do
     @current_user = User.find_by_id(session[:user_id])
     if @current_user
-      erb :account 
+      erb :account
     else
       erb :error
     end 
